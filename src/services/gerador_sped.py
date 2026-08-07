6323d893976870307a923c2cbbc7a23e5289196d
PedroOSanches <pedrohenriqueoliveirasanches3@gmail.com>
2026-08-07 16:17:43 -0300

    fix: Corrigir erro de sintaxe na escrita de linhas no arquivo TXT

--- src/services/gerador_sped.py
+++ src/services/gerador_sped.py
@@ -26,8 +26,7 @@ class GeradorSped:
                 )
 
             for linha in df.itertuples(index=False, name=None):
-                arquivo.write("|" + "|".join("" if pd.isna(valor) else str(valor) for valor in linha) + "|\n"
-)
+                arquivo.write("|" + "|".join("" if pd.isna(valor) else str(valor) for valor in linha) + "|\n")
 
         if type == TipoFormatacao.INVENTARIO:
             df.to_excel(
